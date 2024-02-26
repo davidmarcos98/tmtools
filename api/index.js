44c7cbad258fde255e41ea069017ad34c92d9380
David Marcos <david.mazon@un.org>
2024-02-26 02:32:40 +0100

    new cache headers

--- api/index.js
+++ api/index.js
@@ -29,7 +29,9 @@ function getChatText(data){
 
 app.get('/api/ranks', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 's-max-age=60, stale-while-revalidate');
+    res.setHeader('Cache-Control', 'public, s-maxage=60');
+    res.setHeader('CDN-Cache-Control', 'public, s-max-age=60');
+    res.setHeader('Vercel-CDN-Cache-Control', 'public, s-maxage=120');
 
     let result = {}
     client.campaigns.currentSeason().then(async campaign=>{
